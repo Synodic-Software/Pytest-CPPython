c9ac48958c6d2a8002dcfda84f43b583b33aabaf
Asher Norland <asher.norland@gmail.com>
2022-09-24 23:30:52 -0400

    Reintroduce Version

--- pytest_cppython/fixture_data/pep621.py
+++ pytest_cppython/fixture_data/pep621.py
@@ -3,4 +3,4 @@
 
 from cppython_core.schema import PEP621
 
-pep621_test_list: list[PEP621] = [PEP621(name="default-test")]
+pep621_test_list: list[PEP621] = [PEP621(name="default-test", version="1.0.0")]
